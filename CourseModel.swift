142f0338b23768a3d0348be38f2795b5b7dfeb29
Dirk Milotz <dirkmilotz@Dirks-MacBook-Pro.local>
2017-02-11 00:08:50 -0800

    Real Commit

---
+++ CourseModel.swift
@@ -0,0 +1,45 @@
+//
+//  CourseModel.swift
+//  GolfTourney
+//
+//  Created by Dirk Milotz on 2/8/17.
+//  Copyright © 2017 Dirk Milotz. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+//import Realm
+class golfcourse: Object {
+    dynamic var id = 0
+    dynamic var biz_name = ""
+    dynamic var e_address = ""
+    dynamic var e_city = ""
+    dynamic var e_state = ""
+    dynamic var e_postal = ""
+    dynamic var e_zip_full = ""
+    dynamic var e_country = ""
+    dynamic var loc_county = ""
+    dynamic var loc_area_code = ""
+    dynamic var loc_FIPS = ""
+    dynamic var loc_MSA = ""
+    dynamic var loc_PMSA = ""
+    dynamic var loc_TZ = ""
+    dynamic var loc_DST = ""
+    dynamic var loc_LAT_centroid = ""
+    dynamic var loc_LAT_poly = ""
+    dynamic var loc_LONG_centroid = ""
+    dynamic var loc_LONG_poly = ""
+    dynamic var biz_phone = ""
+    dynamic var metal_spikes = ""
+    dynamic var play_five = ""
+    dynamic var c_holes = ""
+    dynamic var c_type = ""
+    dynamic var year_built = ""
+    dynamic var c_designer = ""
+    dynamic var c_season = ""
+    dynamic var guest_policy = ""
+    dynamic var dress_code = ""
+    dynamic var green_fees = ""
+    dynamic var weekend_rates = ""
+    dynamic var adv_tee = ""
+}
