5bf116e06f0402761bd02de7acadca8c7bd1489d
Dirk Milotz <dmilotz@gmail.com>
2017-03-17 14:37:04 -0700

    Feat: Add more chat functionality to chatViewController

--- GolfTourney/ChatViewController.swift
+++ GolfTourney/ChatViewController.swift
@@ -14,14 +14,66 @@ class ChatViewController: JSQMessagesViewController{
   
   
   /// Properties
+  var ref: FIRDatabaseReference!
+  var game: Game?
+  var chatRef : FIRDatabaseReference! {
+    return ref.child("chats").child((game?.gameId)!)
+  }
+  var newMessageRefHandle: FIRDatabaseHandle?
+ 
   var messages = [JSQMessage]()
   lazy var outgoingBubbleImageView: JSQMessagesBubbleImage = self.setupOutgoingBubble()
   lazy var incomingBubbleImageView: JSQMessagesBubbleImage = self.setupIncomingBubble()
+  
+  func addMessage(withId id: String, name: String, text: String) {
+    if let message = JSQMessage(senderId: id, displayName: name, text: text) {
+      messages.append(message)
+    }
+  }
+  
+  override func viewDidLoad(){
+    super.viewDidLoad()
+
+    self.senderId = FIRAuth.auth()?.currentUser?.uid
+    self.senderDisplayName = "Blah"
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    // messages from someone else
+    addMessage(withId: "foo", name: "Mr.Bolt", text: "I am so fast!")
+    // messages sent from local sender
+    addMessage(withId: senderId, name: "Me", text: "I bet I can run faster than you!")
+    addMessage(withId: senderId, name: "Me", text: "I like to run!")
+    // animates the receiving of a new message on the view
+    finishReceivingMessage()
+  }
   
 }
 
 // MARK: - JSQMessage functions
 extension ChatViewController{
+  private func observeMessages() {
+    let messageQuery = chatRef.queryLimited(toLast:25)
+    
+    // 2. We can use the observe method to listen for new
+    // messages being written to the Firebase DB
+    newMessageRefHandle = messageQuery.observe(.childAdded, with: { (snapshot) -> Void in
+      // 3
+      let messageData = snapshot.value as! Dictionary<String, String>
+      
+      if let id = messageData["senderId"] as String!, let name = messageData["senderName"] as String!, let text = messageData["text"] as String!, text.characters.count > 0 {
+        // 4
+        self.addMessage(withId: id, name: name, text: text)
+        
+        // 5
+        self.finishReceivingMessage()
+      } else {
+        print("Error! Could not decode message data")
+      }
+    })
+  }
+  
   override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
     return messages[indexPath.item]
   }
@@ -41,6 +93,21 @@ extension ChatViewController{
   
   override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
     return nil
+  }
+  
+  override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
+    let itemRef = chatRef.childByAutoId() // 1
+    let messageItem = [ // 2
+      "senderId": senderId!,
+      "senderName": senderDisplayName!,
+      "text": text!,
+      ]
+    
+    itemRef.setValue(messageItem) // 3
+    
+    JSQSystemSoundPlayer.jsq_playMessageSentSound() // 4
+    
+    finishSendingMessage()
   }
   
  func setupOutgoingBubble() -> JSQMessagesBubbleImage {
