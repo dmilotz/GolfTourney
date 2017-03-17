82afeeb9c74d85e4c100b192b8e4b04e13beea84
Dirk Milotz <dmilotz@gmail.com>
2017-03-16 21:40:47 -0700

    feat: firebase chat functionality

---
+++ GolfTourney/ChatViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ChatViewController.swift
+//  GolfTourney
+//
+//  Created by Dirk Milotz on 3/16/17.
+//  Copyright © 2017 Dirk Milotz. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import JSQMessagesViewController
+
+class ChatViewController: JSQMessagesViewController{
+  
+  
+  /// Properties
+  var messages = [JSQMessage]()
+  lazy var outgoingBubbleImageView: JSQMessagesBubbleImage = self.setupOutgoingBubble()
+  lazy var incomingBubbleImageView: JSQMessagesBubbleImage = self.setupIncomingBubble()
+  
+}
+
+// MARK: - JSQMessage functions
+extension ChatViewController{
+  override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+    return messages[indexPath.item]
+  }
+  
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return messages.count
+  }
+  
+  override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+    let message = messages[indexPath.item] // 1
+    if message.senderId == senderId { // 2
+      return outgoingBubbleImageView
+    } else { // 3
+      return incomingBubbleImageView
+    }
+  }
+  
+  override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+    return nil
+  }
+  
+ func setupOutgoingBubble() -> JSQMessagesBubbleImage {
+    let bubbleImageFactory = JSQMessagesBubbleImageFactory()
+    return bubbleImageFactory!.outgoingMessagesBubbleImage(with: UIColor.jsq_messageBubbleBlue())
+  }
+  
+  func setupIncomingBubble() -> JSQMessagesBubbleImage {
+    let bubbleImageFactory = JSQMessagesBubbleImageFactory()
+    return bubbleImageFactory!.incomingMessagesBubbleImage(with: UIColor.jsq_messageBubbleLightGray())
+  }
+  
+  
+  
+}
