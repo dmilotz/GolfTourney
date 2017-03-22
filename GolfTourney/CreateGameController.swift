fc38c44b0c096279a7a5c117d28972bb6f183d6d
Dirk Milotz <dmilotz@gmail.com>
2017-03-21 18:32:12 -0700

    feat: Add default golf slogans when no title is selected for game
    Fun golf slogans are randomly selected to help differentiate games at
    same course

--- GolfTourney/CreateGameController.swift
+++ GolfTourney/CreateGameController.swift
@@ -23,6 +23,7 @@ class CreateGameController: UIViewController{
   var curFromDate : Date? = Date()
   let daysToAdd : TimeInterval = 10
   var photoUrl : URL?
+  var golfSlogans : [String] = ["Grip it and rip it", "Clubbing at its best!", "Golf strong, go long", "Play the course not your opponent", "Arnie would do it", "A bad attitude is worse than a bad swing.", "Don’t fear the bogey man.", "The most important shot in golf is the next one."]
   
   lazy var formatter: DateFormatter = {
     var tmpFormatter = DateFormatter()
@@ -190,8 +191,15 @@ private extension CreateGameController{
     }else{
       websiteString = ""
     }
+    var titleText: String = ""
+    if tourneyTitle.text == "Enter Tourney Title"{
+      let randomNum: Int = Int(arc4random_uniform(UInt32(golfSlogans.count)))
+      titleText = golfSlogans[randomNum]
+    }else{
+      titleText = tourneyTitle.text
+    }
     
-    let game = Game(gameId: UUID().uuidString, preferredHandicap: chosenHandicap!, courseName: course.biz_name.replacingOccurrences(of: ".", with: ""), courseId: String(course.id), courseAddress: "\(course.e_address), \(course.e_city), \(course.e_state)", date:formatter.string(from: curFromDate!), players: [curUser!: ""], buyIn: buyIn, description: tourneyTitle.text, maxPlayers: 20, currentPlayerCount: 1, currentPot: buyIn, gameOwner: curUser, coursePicUrl: extraCourseInfo?["coursePicUrl"] as? String, courseWebsiteUrl: websiteString)
+    let game = Game(gameId: UUID().uuidString, preferredHandicap: chosenHandicap!, courseName: course.biz_name.replacingOccurrences(of: ".", with: ""), courseId: String(course.id), courseAddress: "\(course.e_address), \(course.e_city), \(course.e_state)", date:formatter.string(from: curFromDate!), players: [curUser!: ""], buyIn: buyIn, description: titleText, maxPlayers: 20, currentPlayerCount: 1, currentPot: buyIn, gameOwner: curUser, coursePicUrl: extraCourseInfo?["coursePicUrl"] as? String, courseWebsiteUrl: websiteString)
     NetworkClient.createGame(game)
   }
   
